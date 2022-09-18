07b65d96dbc8154fa22264c2b0cf42c62e736421
fack <null>
2022-09-18 17:20:53 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("connect to server"); //async fn plug?
+    println!("start chess engine");
+    println!("generating moves + coordinates");
+    println!("translate coordinates into vibe notation")
+}
